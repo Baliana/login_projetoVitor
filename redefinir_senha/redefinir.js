6c36f662aea9eefff9c252e8a31234d7d521d4af
FERNANDO BALIANA DE MIRANDA <24122781@CFP127ALUNO.LOCAL>
2025-04-22 15:45:10 -0300

    js redefinir

--- redefinir_senha/redefinir.js
+++ redefinir_senha/redefinir.js
@@ -6,3 +6,34 @@ async function redefinir_senha() {
     const loginUsuario = document.getElementById('recUsuario').value
     const keywordUsuario = document.getElementById('keyword').value
 }
+
+
+async function putPassword(id, senha){
+    const dados = {
+        senha: senha
+    }
+    try {
+        const response = await fetch(`https://back-spider.vercel.app/user/newPassword/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            
+            body: JSON.stringify(
+                dados
+            )
+        })
+
+        console.log(response);
+        
+
+        if (!response.ok) {
+            throw new Error('Erro ao login do usario')
+        }
+
+        return await response.json()
+    } catch (error) {
+        console.error(error)
+        return null
+    }
+}
